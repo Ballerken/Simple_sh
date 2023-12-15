3e88755bae689f680cf54a669374e3df611ddb78
Kennedy <bennettbutt100@gmail.com>
2023-12-15 10:28:55 -0800

    token repair

--- tokens.c
+++ tokens.c
@@ -9,43 +9,43 @@
 
 char **strtow(char *str, char *d)
 {
-        int i, j, k, m, num_words = 0;
-        char **c;
+	int i, j, k, m, num_words = 0;
+	char **c;
 
-        if (str == NULL || str[0] == 0)
-                return (NULL);
-        if (!d)
-                d = " ";
-        for (i = 0; str[i] != '\0'; i++)
-                if (!if_del(str[i], d) && (if_del(str[i + 1], d) || !str[i + 1]))
-                        num_words++;
+	if (str == NULL || str[0] == 0)
+		return (NULL);
+	if (!d)
+		d = " ";
+	for (i = 0; str[i] != '\0'; i++)
+		if (!if_del(str[i], d) && (if_del(str[i + 1], d) || !str[i + 1]))
+			num_words++;
 
-        if (num_words == 0)
-                return (NULL);
-        c = malloc((1 + num_words) * sizeof(char *));
-        if (!c)
-                return (NULL);
-        for (i = 0, j = 0; j < num_words; j++)
-        {
-                while (if_del(str[i], d))
-                        i++;
-                k = 0;
-                while (!if_del(str[i + k], d) && str[i + k])
-                        k++;
-                c[j] = malloc((k + 1) * sizeof(char));
-                if (!c[j])
-                {
-                        for (k = 0; k < j; k++)
-                                free(c[k]);
-                        free(c);
-                        return (NULL);
-                }
-                for (m = 0; m < k; m++)
-                        c[j][m] = str[i++];
-                c[j][m] = 0;
-        }
-        c[j] = NULL;
-        return (c);
+	if (num_words == 0)
+		return (NULL);
+	c = malloc((1 + num_words) * sizeof(char *));
+	if (!c)
+		return (NULL);
+	for (i = 0, j = 0; j < num_words; j++)
+	{
+		while (if_del(str[i], d))
+			i++;
+		k = 0;
+		while (!if_del(str[i + k], d) && str[i + k])
+			k++;
+		c[j] = malloc((k + 1) * sizeof(char));
+		if (!c[j])
+		{
+			for (k = 0; k < j; k++)
+				free(c[k]);
+			free(c);
+			return (NULL);
+		}
+		for (m = 0; m < k; m++)
+			c[j][m] = str[i++];
+		c[j][m] = 0;
+	}
+	c[j] = NULL;
+	return (c);
 }
 
 
@@ -57,18 +57,18 @@ char **strtow(char *str, char *d)
  */
 char **strtoow(char *str, char d)
 {
-	int i, j, k, m, num_words = 0; // Changed variable name from numwords to num_words
-	char **c; // Changed variable name from s to c
+	int i, j, k, m, num_words = 0;
+	char **c;
 
 	if (str == NULL || str[0] == 0)
 		return (NULL);
 	for (i = 0; str[i] != '\0'; i++)
 		if ((str[i] != d && str[i + 1] == d) ||
 				(str[i] != d && !str[i + 1]) || str[i + 1] == d)
-			num_words++; // Changed variable name from numwords to num_words
+			num_words++;
 	if (num_words == 0)
 		return (NULL);
-	c = malloc((1 + num_words) * sizeof(char *)); // Changed variable name from s to c
+	c = malloc((1 + num_words) * sizeof(char *));
 	if (!c)
 		return (NULL);
 	for (i = 0, j = 0; j < num_words; j++)

15f649281b0e945c4f4aac6308fd612305eefcb1
Kennedy <bennettbutt100@gmail.com>
2023-12-15 08:47:08 -0800

    token

--- tokens.c
+++ tokens.c
@@ -9,44 +9,45 @@
 
 char **strtow(char *str, char *d)
 {
-	int i, j, k, m, numwords = 0;
-	char **s;
+        int i, j, k, m, num_words = 0;
+        char **c;
 
-	if (str == NULL || str[0] == 0)
-		return (NULL);
-	if (!d)
-		d = " ";
-	for (i = 0; str[i] != '\0'; i++)
-		if (!if_del(str[i], d) && (if_del(str[i + 1], d) || !str[i + 1]))
-			numwords++;
+        if (str == NULL || str[0] == 0)
+                return (NULL);
+        if (!d)
+                d = " ";
+        for (i = 0; str[i] != '\0'; i++)
+                if (!if_del(str[i], d) && (if_del(str[i + 1], d) || !str[i + 1]))
+                        num_words++;
 
-	if (numwords == 0)
-		return (NULL);
-	s = malloc((1 + numwords) * sizeof(char *));
-	if (!s)
-		return (NULL);
-	for (i = 0, j = 0; j < numwords; j++)
-	{
-		while (if_del(str[i], d))
-			i++;
-		k = 0;
-		while (!if_del(str[i + k], d) && str[i + k])
-			k++;
-		s[j] = malloc((k + 1) * sizeof(char));
-		if (!s[j])
-		{
-			for (k = 0; k < j; k++)
-				free(s[k]);
-			free(s);
-			return (NULL);
-		}
-		for (m = 0; m < k; m++)
-			s[j][m] = str[i++];
-		s[j][m] = 0;
-	}
-	s[j] = NULL;
-	return (s);
+        if (num_words == 0)
+                return (NULL);
+        c = malloc((1 + num_words) * sizeof(char *));
+        if (!c)
+                return (NULL);
+        for (i = 0, j = 0; j < num_words; j++)
+        {
+                while (if_del(str[i], d))
+                        i++;
+                k = 0;
+                while (!if_del(str[i + k], d) && str[i + k])
+                        k++;
+                c[j] = malloc((k + 1) * sizeof(char));
+                if (!c[j])
+                {
+                        for (k = 0; k < j; k++)
+                                free(c[k]);
+                        free(c);
+                        return (NULL);
+                }
+                for (m = 0; m < k; m++)
+                        c[j][m] = str[i++];
+                c[j][m] = 0;
+        }
+        c[j] = NULL;
+        return (c);
 }
+
 
 /**
  * **strtoow - splits a string into words
